0d0525f0a475e6f04e872a73629065daf0bf4fd0
Cannotheat <bo23519@gmail.com>
2021-04-08 22:46:18 -0400

    Update saveArchitecture.py
    update the parse string method to adapt what is on Slack

--- saveArchitecture.py
+++ saveArchitecture.py
@@ -13,11 +13,43 @@ class saveArchitecture:
     def toString(self, list):
         result = ""
         for item in list:
-            name, shape = item
-            result += name
-            for i in shape:
-                result += "(" + str(i) + ") "
-            result += "---"
+            result += "|"
+            name = item[0]
+
+            if name is "convolution":
+                nfilters = item[1]
+                filter_size = item[2]
+                stride = item[3]
+                representation_size = item[4]
+                layer_depth = item[5]
+                result += str(nfilters) \
+                        + "," + str(filter_size) \
+                        + "," + str(stride) \
+                        + "," + str(representation_size) \
+                        + "," + str(layer_depth)
+
+            elif name is "pooling":
+                pool_size = item[1]
+                stride = item[2]
+                layer_depth = item[3]
+                result += str(pool_size) \
+                        + "," + str(stride) \
+                        + "," + str(layer_depth)
+
+            elif name is "dense":
+                nnodes = item[1]
+                layer_depth = item[2]
+                consecutive_FC = item[3]
+                result += str(nnodes) \
+                        + "," + str(layer_depth) \
+                        + "," + str(consecutive_FC)
+
+            elif name is "softmax":
+                nnodes = item[1]
+                layer_depth = item[2]
+                result += str(nnodes) \
+                        + "," + str(layer_depth)
+
         return result
 
     def getValue(self, list):
